e1068f37a530e3e2377f072872751cb8410b6ddb
Caroline Setiawan <caroline.s@192-168-1-7.tpgi.com.au>
2018-06-04 23:20:02 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank:"king",
+	suit:"hearts",
+	cardImage:"images/king-of-hearts.png"
+},
+{
+	rank:"king",
+	suit:"diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -13,8 +35,11 @@ var checkForMatch = function() {
 
 var flipCard = function (cardId) {
 	checkForMatch();
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	var flippedCard = cards[cardId];
+	console.log("User flipped " + flippedCard.rank);
+	console.log(flippedCard.suit);
+	console.log(flippedCard.cardImage);
+	cardsInPlay.push(flippedCard.rank);
 };
 
 flipCard(0);
